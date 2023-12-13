32badac051ac18e038784113b2107778bb9bd01b
someoneinjd <someoneinjd@outlook.com>
2023-12-13 16:53:29 +0800

    Add day13 (python)

---
+++ python/day13.py
@@ -0,0 +1,24 @@
+def find_diff(pattern: list[str], diff=0) -> int:
+    for i in range(len(pattern) - 1):
+        pos_len = min(i + 1, len(pattern) - i - 1)
+        if (
+            sum(
+                sum(pattern[i - x][y] != pattern[i + 1 + x][y] for y in range(len(pattern[0])))
+                for x in range(pos_len)
+            )
+            == diff
+        ):
+            return i + 1
+    return 0
+
+
+def count(pattern: list[str], diff=0) -> int:
+    return 100 * find_diff(pattern, diff) + find_diff(
+        ["".join(pattern[j][i] for j in range(len(pattern))) for i in range(len(pattern[0]))], diff
+    )
+
+
+patterns = open("./dataset/day13.txt").read().strip()
+
+print(sum(count(pattern.splitlines()) for pattern in patterns.split("\n\n")))
+print(sum(count(pattern.splitlines(), 1) for pattern in patterns.split("\n\n")))
